b089c3b7052ec24fad50fff243de8ba66cc2c99e
Diego Mijelshon <diego@mijelshon.com.ar>
2013-02-25 16:14:29 -0300

    Initial commit
    Copied from latest nhcontrib and used NuGet for references.

---
+++ Caches/SysCache/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Security;
+
+[assembly: CLSCompliantAttribute(true)]
+[assembly: AssemblyTitleAttribute("NHibernate.Caches.SysCache")]
+[assembly: AssemblyDescriptionAttribute("Cache provider for NHibernate using ASP.NET Cache object.")]
+[assembly: AssemblyCompanyAttribute("NHibernate.org")]
+[assembly: AssemblyProductAttribute("NHibernate.Caches.SysCache")]
+[assembly: AssemblyCopyrightAttribute("Licensed under LGPL.")]
+[assembly: AssemblyVersionAttribute("3.1.0.4000")]
+[assembly: AssemblyInformationalVersionAttribute("3.1.0.4000")]
+[assembly: AssemblyFileVersionAttribute("3.1.0.4000")]
+//[assembly: AssemblyKeyFileAttribute("../../src/NHibernate.snk")]
+[assembly: AssemblyDelaySignAttribute(false)]
+[assembly: AllowPartiallyTrustedCallers()]
