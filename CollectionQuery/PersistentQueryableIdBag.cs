aeab171c7834e3d53e23a76d44af2fc5c3c9d10b
Diego Mijelshon <diego@mijelshon.com.ar>
2013-05-09 21:15:10 -0300

    IdBag support

---
+++ CollectionQuery/PersistentQueryableIdBag.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using NHibernate.Collection.Generic;
+using NHibernate.Engine;
+
+namespace NHibernate.CollectionQuery
+{
+    public class PersistentQueryableIdBag<T> : PersistentIdentifierBag<T>, IQueryable<T>
+    {
+        IQueryable queryable;
+
+        public PersistentQueryableIdBag()
+        {
+        }
+
+        public PersistentQueryableIdBag(ISessionImplementor sessionImplementor)
+            : base(sessionImplementor)
+        {
+        }
+
+        public PersistentQueryableIdBag(ISessionImplementor sessionImplementor, ICollection<T> collection)
+            : base(sessionImplementor, collection)
+        {
+        }
+
+        public Expression Expression
+        {
+            get { return GetQueryable().Expression; }
+        }
+
+        public System.Type ElementType
+        {
+            get { return typeof(T); }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { return GetQueryable().Provider; }
+        }
+
+        IQueryable GetQueryable()
+        {
+            return queryable ?? (queryable = WasInitialized ? InternalValues.AsQueryable() : this.Query(Session));
+        }
+    }
+}
---
+++ CollectionQuery/Test/IdBagFixture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using NHibernate.CollectionQuery.Test.Domain;
+using NHibernate.Mapping.ByCode;
+
+namespace NHibernate.CollectionQuery.Test
+{
+    public class IdBagFixture : QueryableCollectionFixture
+    {
+        protected override ICollection<Bar> CreateCollection()
+        {
+            return new List<Bar>();
+        }
+
+        protected override void CustomizeMapping(ConventionModelMapper mapper)
+        {
+            mapper.Class<Foo>(cm => cm.IdBag(x => x.Bars,
+                                             bpm =>
+                                             {
+                                                 bpm.Cascade(Cascade.All);
+                                                 bpm.Type<PersistentQueryableIdBagType<Bar>>();
+                                             },
+                                             cer => cer.ManyToMany()));
+        }
+ 
+    }
+}
